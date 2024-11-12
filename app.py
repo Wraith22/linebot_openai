25f4598d80f0a02a82ab57b0b53d649912b09be1
Wraith22 <mspeter5208@gmail.com>
2024-11-12 15:38:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    if msg == '猜猜我幾歲':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('60'))
     try:
         GPT_answer = GPT_response(msg)
         print(GPT_answer)
